7092bbfa008395df8cea136e7db756df7963b192
Daniel Sharp <dsharpc@gmail.com>
2023-01-24 15:38:58 +0000

    part 2 exercises up to 2.9

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+
+
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    if (persons.some(p => p.name === newName)){
+      alert(`${newName} is already added to phonebook`)
+    }else{
+      setPersons(persons.concat({name : newName, number: newNumber}))
+    }
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const personsToShow = !newSearch ? persons : persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase()))
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <div>
+        filter shown with: <input onChange={(event) => setNewSearch(event.target.value)} value={newSearch} />
+      </div>
+      <h2>Add a new</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={(event) => setNewName(event.target.value)} value={newName}/>
+        </div>
+        <div>
+          number: <input onChange={(event) => setNewNumber(event.target.value)} value={newNumber}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {personsToShow.map(person => <p key={person.name}>{person.name} - {person.number}</p>)}
+    </div>
+  )
+}
+
+export default App
