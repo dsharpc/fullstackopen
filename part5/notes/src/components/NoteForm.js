dea2e2e21d8633f8223df7590bf643bf9c582963
Daniel Sharp <dsharpc@gmail.com>
2023-02-15 15:42:55 +0000

    Completed frontend testing section for notes app

--- part5/notes/src/components/NoteForm.js
+++ part5/notes/src/components/NoteForm.js
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
-const NoteForm = ({ createNote}) => {
+const NoteForm = ({ createNote }) => {
   const [newNote, setNewNote] = useState('')
- 
+
   const addNote = (event) => {
     event.preventDefault()
     createNote({
@@ -14,15 +14,15 @@ const NoteForm = ({ createNote}) => {
   }
 
   return (
-  <div>
-    <h2>Create a new note</h2>
-    <form onSubmit={addNote}>
-    <input value={newNote} onChange={event => setNewNote(event.target.value)} />
-    <button type="submit">save</button>
-    </form>
-  </div>
- )
- 
+    <div className="formDiv">
+      <h2>Create a new note</h2>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={event => setNewNote(event.target.value)} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+
 }
 
 export default NoteForm
