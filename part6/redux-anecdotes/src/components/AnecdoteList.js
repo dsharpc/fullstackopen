37100de9d31f1e98b31c6d2fdcb4260a98e24c04
Daniel Sharp <dsharpc@gmail.com>
2023-02-18 11:57:14 +0000

    completed exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -3,7 +3,11 @@ import { upvoteAnecdote } from '../reducers/anecdoteReducer'
 
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+  const filter = useSelector(state => state.filter)
+  const anecdotes = useSelector(state => {
+    const relevantAnecdotes = state.anecdotes.filter(anecdote => anecdote.content.toLowerCase().includes(filter))
+    return relevantAnecdotes.sort((a, b) => b.votes - a.votes)
+  })
   const dispatch = useDispatch()
 
   return (
